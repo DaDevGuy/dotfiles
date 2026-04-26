c3752c37aff78d043d1de1d0a6b0c946651f8e18
DaDevGuy <me@dadevguy.com>
2026-04-26 22:24:06 +0100

    add config and update sway config

--- sway/status.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while true; do
-  TRACK=$(playerctl metadata --format "[{{title}} - {{artist}}]" 2>/dev/null || echo "")
-  DATE=$(date "+[%d-%m-%Y %H:%M:%S]")
-  echo "${TRACK:+$TRACK}$DATE"
-  sleep 1
